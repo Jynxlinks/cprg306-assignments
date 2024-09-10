671834927271abff59c7affecaea679761826d87
Dominic Cap <152248786+Jynxlinks@users.noreply.github.com>
2024-09-10 14:43:59 -0600

    complete part 2 of Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+   return (
+   <main>
+    <h1>Dominic Cap</h1>
+  
+    <ul>
+      <li>
+       <Link href="https://github.com/Jynxlinks?tab=repositories">https://github.com/Jynxlinks?tab=repositories</Link>
+      </li>
+    </ul>
+  </main>
+  );
+}
